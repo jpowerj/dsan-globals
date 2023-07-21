889c9924d629ee0f8d16610d043f83e495e593a0
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-07-20 22:46:54 -0400

    Removed _r.globals.qmd, since the path to tex2pdf.tex is different between website and book

--- compile_globals.ps1
+++ compile_globals.ps1
@@ -1,24 +1,13 @@
-$d5000_path = "../dsan5000"
-$dsci_path = "../data-science"
-$d5100_path = "../dsan5100"
-$pstats_path = "../prob-stats"
-Copy-Item "./Georgetown_Refs.bib" "$d5000_path/assets/data/Georgetown_Refs.bib"
-Copy-Item "./Georgetown_Refs.bib" "$d5100_path/assets/data/Georgetown_Refs.bib"
-Copy-Item "./ggplot_globals.r" "$d5000_path/assets/code/ggplot_globals.r"
-Copy-Item "./ggplot_globals.r" "$d5100_path/assets/code/ggplot_globals.r"
-Copy-Item "./globals.r" "$d5000_path/assets/code/globals.r"
-Copy-Item "./globals.r" "$d5100_path/assets/code/globals.r"
-Copy-Item "./jjslides.scss" "$d5000_path/assets/css/jjslides.scss"
-Copy-Item "./jjslides.scss" "$d5100_path/assets/css/jjslides.scss"
-Copy-Item "./_pw_protect.py" "$d5000_path/_pw_protect.py"
-Copy-Item "./_pw_protect.py" "$d5100_path/_pw_protect.py"
-Copy-Item "./_r.globals.qmd" "$d5000_path/_r.globals.qmd"
-Copy-Item "./_r.globals.qmd" "$d5100_path/_r.globals.qmd"
-Copy-Item "./_revealjs.globals.yml" "$d5000_path/_revealjs.globals.yml"
-Copy-Item "./_revealjs.globals.yml" "$d5100_path/_revealjs.globals.yml"
-Copy-Item "./_tex.globals.qmd" "$d5000_path/_tex.globals.qmd"
-Copy-Item "./_tex.globals.qmd" "$d5100_path/_tex.globals.qmd"
-Copy-Item "./tikz2pdf.tex" "$d5000_path/assets/code/tikz2pdf.tex"
-Copy-Item "./tikz2pdf.tex" "$d5100_path/assets/code/tikz2pdf.tex"
-Copy-Item "./_tikz.setup.txt" "$d5000_path/_tikz.setup.txt"
-Copy-Item "./_tikz.setup.txt" "$d5100_path/_tikz.setup.txt"
+$to_fpaths = "/_bookdown.globals.yml","/assets/data/Georgetown_Refs.bib", "/_gg.globals.qmd", "/assets/css/jjslides.scss", "/_pw_protect.py","/_revealjs.globals.yml", "/_tex.globals.qmd", "/assets/code/tikz2pdf.tex", "/_tikz.setup.txt"
+#$root_dirs = "../dsan5000","../data-science","../dsan5100","../prob-stats"
+$root_dirs = "../dsan5000","../data-science"
+Foreach($to_fpath in $to_fpaths) {
+    #Write-Output "$path"
+    Foreach($root_dir in $root_dirs) {
+        $fname = [System.IO.Path]::GetFileName($to_fpath)
+        $from_fpath = "./$fname"
+        $to_fpath_full = "$root_dir/$to_fpath"
+        Copy-Item $from_fpath $to_fpath_full
+        Write-Output "Copied $from_fpath to $to_fpath_full"
+    }
+}
