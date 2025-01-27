87d6d65277f8011ba96c0f6164812ac92e48ea48
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-01-27 16:16:00 -0500

    trying to merge

--- _globals.r
+++ _globals.r
@@ -6,29 +6,46 @@ cbPalette <- c(
 )
 cb_palette <- cbPalette
 options(ggplot2.discrete.colour = cbPalette)
-# Theme generator, for given sizes
-dsan_theme <- function(plot_type = "full") {
+get_base_size <- function(plot_type) {
   if (plot_type == "full") {
-      custom_base_size <- 16
+    custom_base_size <- 16
   } else if (plot_type == "half") {
-      custom_base_size <- 22
+    custom_base_size <- 22
   } else if (plot_type == "quarter") {
-      custom_base_size <- 28
+    custom_base_size <- 28
   } else {
-      # plot_type == "col"
-      custom_base_size <- 22
+    # plot_type == "col" # nolint: commented_code_linter.
+    custom_base_size <- 22
   }
-  theme <- theme_classic(base_size = custom_base_size) +
-      theme(
-          plot.title = element_text(hjust = 0.5),
-          plot.subtitle = element_text(hjust = 0.5),
-          legend.title = element_text(hjust = 0.5),
-          legend.box.background = element_rect(colour = "black")
-      )
-  return(theme)
+  return(custom_base_size)
 }
-
+# Theme generator, for given sizes
+dsan_theme_settings <- theme(
+  plot.title = element_text(hjust = 0.5),
+  plot.subtitle = element_text(hjust = 0.5),
+  legend.title = element_text(hjust = 0.5),
+  legend.box.background = element_rect(colour = "black")
+)
+dsan_theme <- function(plot_type = "full", base_size = NA) {
+  if (is.na(base_size)) {
+    base_size <- get_base_size(plot_type)
+  }
+  custom_theme <- theme_classic(
+    base_size = base_size
+  ) + dsan_theme_settings
+  return(custom_theme)
+}
 theme_dsan <- dsan_theme
+
+# Minimal version
+theme_dsan_min <- function(base_size = 22) {
+  custom_theme_min <- theme_minimal(
+    base_size = base_size
+  ) + dsan_theme_settings + theme(
+    panel.grid = element_blank()
+  )
+  return(custom_theme_min)
+}
 
 knitr::opts_chunk$set(fig.align = "center")
 g_pointsize <- 5
@@ -55,6 +72,10 @@ remove_legend <- function() {
 # Global functions
 get_rss <- function(model) {
   return(sum(summary(model)$residuals^2))
+}
+
+fmt_decimal <- function(value) {
+  return(sprintf("%0.10f", value))
 }
 
 # https://www.pmassicotte.com/posts/2022-08-15-removing-whitespace-around-figures-quarto/
